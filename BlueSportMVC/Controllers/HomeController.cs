05aadc590153b42e89a53fd88897dd70c4e8db90
duccanh07 <leduccanh.it@gmail.com>
2021-07-31 13:04:33 +0700

    Add _PartialCommitment

--- BlueSportMVC/Controllers/HomeController.cs
+++ BlueSportMVC/Controllers/HomeController.cs
@@ -33,6 +33,8 @@ namespace BlueSportMVC.Controllers
                 lsBannerFooter = GetBannersFooter(),
 
                 lsVisit = GetVisits(),
+
+                lsCommitment = GetCommitments()
             };
             return View("~/Views/Home/Index.cshtml", vm);
         }
@@ -1230,7 +1232,37 @@ namespace BlueSportMVC.Controllers
 
             return data;
         }
-
+        private List<BlueSportMVC.Models.CommitmentModel> GetCommitments()
+        {
+            var data = new List<Models.CommitmentModel>();
+            data.Add(new Models.CommitmentModel()
+            {
+                Icon = "icon-chinhhang",
+                Title = "Chính hãng 100%",
+                Detail = "Hơn 100 thương hiệu nổi tiếng"
+            });
+            data.Add(new Models.CommitmentModel()
+            {
+                Icon = "icon-mpgiaohang",
+                Title = "Miễn phí giao hàng",
+                Detail = "Cho đơn hàng trên 500.000đ"
+            });
+            data.Add(new Models.CommitmentModel()
+            {
+                Icon = "icon-doi1",
+                Title = "1 đổi 1 trong vòng 1 năm",
+                Detail = "Tại 200 cửa hàng trên toàn quốc",
+                PathText = "Xem chi tiết",
+                PathUrl = "#"
+            });
+            data.Add(new Models.CommitmentModel()
+            {
+                TextIcon = "0%",
+                Title = "Trả góp 0%",
+                Detail = "Qua thẻ hoặc duyệt hồ sơ với mọi sản phẩm trên 1.5 triệu",
+            });
+            return data;
+        }
 
         private List<BlueSportMVC.Models.BannerModel> GetBannersFooter()
         {
--- BlueSportMVC/ViewModels/vmHome.cs
+++ BlueSportMVC/ViewModels/vmHome.cs
@@ -10,20 +10,16 @@ namespace BlueSportMVC.ViewModels
     {
 
         public List<BannerModel> lsBanner { get; internal set; }
-
         public List<VisitModel> lsVisit { get; set; }
-
         public List<ProductModel> lsProductAll { get; set; }
-
         public List<ProductModel> lsProductMale { get; set; }
         public List<ProductModel> lsProductFemale { get; set; }
-
         public List<ProductModel> lsProductChildren { get; set; }
         public List<ProductModel> lsProductBicycle { get; set; }
-
         public List<ProductModel> lsProductSport { get; set; }
         public List<ListBrandModel> lsBrand { get; internal set; }
         public List<BannerModel> lsBannerFooter { get; internal set; }
+        public List<CommitmentModel> lsCommitment { get; set; }
 
 
     }
